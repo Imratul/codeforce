bbd7b5cce09efa64b304c2e3b1097005c9de1e40
Imratul <56705202+Imratul@users.noreply.github.com>
2020-04-19 12:28:41 +0600

    110A Nearly Lucky Number

---
+++ Codeforces_Nearly Lucky Number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+ long long n,count=0;
+ cin>>n;
+ while(n!=0){
+
+    if(n%10==4 || n%10==7){
+        count++;
+    }
+    n=n/10;
+
+ }
+
+ if(count==4 || count==7)
+        cout<<"YES";
+    else
+        cout<<"NO";
+
+
+
+
+}
